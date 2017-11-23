64116580f06237a0dc500721b8da41b781a5c6aa
Ricardo Arango <raarangoq@unal.edu.co>
2017-11-22 23:34:53 -0500

    codigo consolidado

--- Goal&Pledged.py
+++ Goal&Pledged.py
@@ -1,55 +1,93 @@
 
 # coding: utf-8
 
-# In[147]:
+# In[1]:
 
 # Importar librerías necesarias
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings('ignore')
 
 
-# In[148]:
+# In[2]:
 
 # Leer datos
 
-file = pd.read_csv("consolidado.csv", skiprows = 0, sep = ';', header=0)
+data = pd.read_csv("consolidado.csv", skiprows = 0, sep = ';', header=0)
+data.head(100)
 
 
-# In[149]:
+# In[3]:
 
 # Calcular la cantidad de proyectos
 
-a = len(file)
+x = len(data)
+x
+
+
+# In[4]:
+
+# Crear subonjunto de datos solo con US
+
+data_only_US = data.groupby('country')
+new_data = data.loc[data_only_US.groups['US']]
+new_data.head(30)
+
+
+# In[5]:
+
+# Calcular la cantidad de proyectos de US
+
+a = len(new_data)
 a
 
 
-# In[150]:
+# In[6]:
 
-# Crear nuevo dataframe con las columnas goal, pledged y state
+# Crear nuevo dataframe con las columnas goal, pledged, state y category
 
-new_file = file.loc[:,['goal','pledged','state']]
-new_file.head(30)
+new_data2 = new_data.loc[:,['goal','pledged','state', 'category']]
+new_data2
 
 
-# In[151]:
+# In[7]:
 
 # Crear subonjunto de datos del nuevo dataframe para state = successful
 
-new_file_state = new_file.groupby('state')
-new_file_state_successful = new_file.loc[new_file_state.groups['successful']]
-new_file_state_successful.head(30)
+new_data3 = new_data2.groupby('state')
+new_data_successful = new_data2.loc[new_data3.groups['successful']]
+new_data_successful.head(30)
 
 
-# In[152]:
+# In[8]:
 
 # Calcular número de proyectos successful
 
-#b = new_file_state_successful['state'].value_counts()
-len(new_file_state_successful)
+b = len(new_data_successful)
+b
 
 
-# In[153]:
+# In[9]:
+
+# Crear subonjunto de datos del nuevo dataframe para state = failed
+
+new_data4 = new_data2.groupby('state')
+new_data_failed = new_data2.loc[new_data4.groups['failed']]
+new_data_failed.head(30)
+
+
+# In[10]:
+
+# Calcular número de proyectos failed
+
+c = len(new_data_failed)
+c
+
+
+# In[11]:
 
 # Calcular el porcentaje de proyectos successful
 
@@ -57,48 +95,90 @@ percentage_of_successful_projects = b/a
 percentage_of_successful_projects
 
 
-# In[154]:
+# In[12]:
+
+# Calcular el porcentaje de proyectos failed
+
+percentage_of_failed_projects = c/a
+percentage_of_failed_projects
+
+
+# In[13]:
+
+#Convertir columnas goal y pledged en enteros
+
+new_data_successful['goal'] = pd.to_numeric(new_data_successful['goal']).astype(int)
+new_data_successful['pledged'] = pd.to_numeric(new_data_successful['pledged']).astype(int)
+
+new_data_successful
+
+
+# In[14]:
 
 # Calcular la diferencia entre las columnas goal y pledged
 
-new_file_state_successful['difference'] = new_file_state_successful['pledged'] - new_file_state_successful['goal'] 
-new_file_state_successful.head(30)
+new_data_successful['difference'] = new_data_successful['pledged'] - new_data_successful['goal'] 
+new_data_successful.head(30)
 
 
-# In[155]:
-
-# Ordenar descendentemente
-
-new_file_state_successful.sort_values('difference', ascending = False)
-
-
-# In[156]:
+# In[15]:
 
 # Extraer difference mayor que cero
 
-c = new_file_state_successful.loc[new_file_state_successful['difference'] > 0]
-len(c)
+d = new_data_successful.loc[new_data_successful['difference'] > 0]
+e = len(d)
+e
 
 
-# In[157]:
+# In[16]:
 
-# Calcular el número y porcentaje de proyectos successful con difference mayor que cero 
+# Calcular proporción de proyectos con difference mayor que cero 
 
-projects_number_0 = len(new_file_state_successful) - len(c)
-percentage_of_successful_projects_0 = len(c) / len(new_file_state_successful)
-print(projects_number_0)
-print(percentage_of_successful_projects_0)
+f = e/b
+f
 
 
-# In[158]:
+# In[17]:
 
-# Ordenar descendentemente difference mayor que cero 
+#Pintar gráfico de proyectos con difference mayor que cero y menor que cero
 
-d = c.sort_values('difference', ascending = False)
-d
+labels = 'Sobrepasan la meta', 'No sobrepasan la meta'
+sizes = [(f),(1-f)]
+explode = (0.1, 0)
+
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
+ax1.axis('equal')
+plt.title('Proyectos exitosos de Estados Unidos')
+plt.show()
 
 
-# In[159]:
+# In[18]:
+
+# Ordenar descendentemente
+
+d.sort_values('difference', ascending = False)
+
+
+# In[19]:
+
+# Dividir en rangos la columna difference y agregar una nueva columna range - Pintar
+
+labels=['[0%-25%]', '(25%-75%]', '(75%-95%]', '(95%-100%]']
+
+d['range']=pd.qcut(d['difference'], [0, .25, .75, .95, 1.],labels=labels)
+
+plt.figure()
+bin_diference=d[['range','difference']]
+bin_diference=bin_diference.groupby('range').count()
+bin_diference.plot.bar(figsize=(10, 6),color='g')
+plt.title('Sobrepasos por cuantiles')
+plt.xlabel('Cuantiles')
+plt.ylabel('Número de proyectos')
+plt.show()
+
+
+# In[20]:
 
 # Calcular ratio de difference sobre goal
 
@@ -106,21 +186,30 @@ d['ratio'] = d['difference'] / d['goal']
 d.head(30)
 
 
-# In[160]:
+# In[23]:
 
-d['goal'] = pd.to_numeric(d['goal']).astype(float)
-d['pledged'] = pd.to_numeric(d['pledged']).astype(float)
-d['difference'] = pd.to_numeric(d['difference']).astype(float)
-d
+# Pintar muestra de cantidad de veces que se sobrepasan los proyectos
 
+bin_ratio=d[['ratio', 'ratio']]
+bin_ratio.columns=['ratio', 'ratio2']
 
-# In[161]:
+bin_ratio=round(bin_ratio, 0)
 
-d['ratio'] = d['difference'] / d['goal'] 
-d.head(30)
+bin_ratio=bin_ratio[bin_ratio['ratio']>0]
+bin_ratio=bin_ratio[bin_ratio['ratio']<50]
 
+bin_ratio=bin_ratio.groupby('ratio').count()
 
-# In[ ]:
+bin_ratio=bin_ratio[bin_ratio['ratio2']<5000]
 
+frames = [pd.DataFrame(list(bin_ratio.index)), bin_ratio['ratio2']]
+bin_ratio4 = pd.concat(frames, axis=1)
 
+bin_ratio4.columns=['ratio', 'ratio2']
+bin_ratio4.plot.scatter(x='ratio',y='ratio2', figsize=(10, 6),color='g')
+
+plt.title('Cantidad de veces que se sobrepasan los proyectos')
+plt.xlabel('Cantidad')
+plt.ylabel('Número de proyectos')
+plt.show()
 
--- consolidar.py
+++ consolidar.py
@@ -6,13 +6,16 @@ Created on Sat Nov 18 16:48:41 2017
 @author: ricardo
 """
 
-
+# Paquetes a importar, para manejar los datos en un dataframe
 import glob
-import pandas 
+import pandas
+# Paquetes para descargar el archivo de la web y descomprimirlo
+import requests, zipfile, io
 
-from datetime import datetime
 
 def ReadFile(path):
+    # Dada la ruta de un archivo, se lee este, y se separan ciertas columnas de interés para analizar
+    # no todas las columnas tienen información relevante
     file = pandas.read_csv(path, skiprows = 0, sep = ',', header=0,
                  usecols = ['id', 'name', 'blurb', 'goal',
                             'state', 'pledged', 'state', 'country',
@@ -20,36 +23,77 @@ def ReadFile(path):
                             'backers_count', 'creator', 'location',
                             'category', 'spotlight', 'staff_pick'])
 
-    file['location'] = file['location'].str.split('displayable_name').str[1]
-    file['location'] = file['location'].str.split('"').str[2]
-    
-    file['creator'] = file['creator'].str.split(':').str[10]
-    file['creator'] = file['creator'].str.split(',').str[0]
-    
-    file['category'] = file['category'].str.split(':').str[-1]
-    file['category'] = file['category'].str.split('"').str[1]
-    
-    file['subcategory'] = file['category'].str.split('/').str[1]
-    file['category'] = file['category'].str.split('/').str[0]
-     
-    file['deadline']=pandas.to_datetime(file['deadline'], unit='s')
-    file['launched_at']=pandas.to_datetime(file['launched_at'], unit='s')
-    
-    file['blurb'] = file['blurb'].replace(';', '.')
-    
+    # se filtran los proyectos por aquellos que fueron exitoso o fallidos
+    # pues el objetivo es predecir si un poryecto será exitoso o no
     file.drop( 
             file[(file['state'] != 'successful') & (file['state'] != 'failed') ].index, 
             inplace = True)
+    
+    # Dado que mas del 80% de los proyectos son de los Estados Unidos, y para evitar problemas de conversión
+    # entre monedas, se opta por solo operar con este país
+    file.drop( 
+            file[(file['country'] != 'US')].index, 
+            inplace = True)
+    
+    # La columna location tiene un formato json con muchos campos innecesarios
+    # se debe separar la información pedida, en este caso la ciudad en donde se realizó el proyecto
+    file['location'] = file['location'].str.split('displayable_name').str[1]
+    file['location'] = file['location'].str.split('"').str[2]
+    
+    # La columna creator tiene un formato json con muchos campos innecesarios
+    # se debe separar la información pedida, en este caso el nombre de la persona o entidad que creó el proyecto
+    file['creator'] = file['creator'].str.split(':').str[10]
+    file['creator'] = file['creator'].str.split(',').str[0]
+    
+    # la columna category tambien tiene un formato json con campos innecesarios, se separa la categoría del proyecto
+    file['category'] = file['category'].str.split(':').str[-1]
+    file['category'] = file['category'].str.split('"').str[1]
+    # Tambien se obtiene la subcategoría de cada proyecto
+    file['subcategory'] = file['category'].str.split('/').str[1]
+    file['category'] = file['category'].str.split('/').str[0]
+    
+    # Los campos deadline (que indica el momento en que terminó el proyecto) y 
+    # launched_at (que indica el momento en que inició un proyecto) están en formato timestamp de unix
+    # así que se deben transformar a una fecha legible
+    file['deadline']=pandas.to_datetime(file['deadline'], unit='s')
+    file['launced_at']=pandas.to_datetime(file['launched_at'], unit='s')
+    
+    # Este campo tiene la descripción del proyecto, para evitar problemas de lectura posteriores
+    # se reemplazan los ; por .
+    file['blurb'] = file['blurb'].replace(';', '.')
   
+    file.reset_index(inplace=True, drop = True)
+    # Agregar la duración del proyecto en días en la plataforma, para analizar tiempos de duración
+    # de un proyecto
+    file['time'] = 0
+    for i in range(0, len(file)):
+        inicio = file.loc[i, 'launched_at']
+        fin = file.loc[i, 'deadline']
+        
+        file.loc[i, 'time'] = (fin - inicio).days
+
     return file
 
+# Se descarga el archivo .zip de la web del autor y se descomprime en la carpeta actual
+# Descarga y descompresion de los archivos
+'''
+url = "https://s3.amazonaws.com/weruns/forfun/Kickstarter/Kickstarter_2017-10-15T10_20_38_271Z.zip"
+r = requests.get(url, stream=True)
+z = zipfile.ZipFile(io.BytesIO(r.content))
+z.extractall()
+'''
 
+
+# Se toma el nombre de todos los archivos .csv que existan en la carpeta actual
 pathFiles = glob.glob('*.csv')
+# se descarta el archivo consolidado.csv, que será el archivo con todos los datos en uno solo
 if ("consolidado.csv" in pathFiles):
     pathFiles.remove('consolidado.csv')
+    
+# este será el dataframe que contendrá la información
 bigFile = pandas.DataFrame()
     
-# Procesa cada archivo y lo anexa a un solo DataFrame a regresar
+# Procesa cada archivo y lo anexa a un solo DataFrame
 for path in pathFiles:
     print(path)
     bigFile = bigFile.append(ReadFile(path), ignore_index = True)
@@ -61,15 +105,6 @@ bigFile.drop_duplicates(inplace = True)
 # Guardar archivo
 print("Guardando archivo consolidado")
 bigFile.to_csv("consolidado.csv", sep = ";", na_rep = '', index = False)
-
-
-
-
-#linea = bigFile['location']
-
-
-#import pandas 
-#file = pandas.read_csv("consolidado.csv", skiprows = 0, sep = ';', header=0)
 
 
 
